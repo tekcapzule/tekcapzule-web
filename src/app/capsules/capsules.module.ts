4887628360266b42fa6cefa577c494d2d86958b6
Akhil PB <akhil.pb001@gmail.com>
2021-07-31 19:29:21 +0530

    Add tabs in capsules page

--- src/app/capsules/capsules.module.ts
+++ src/app/capsules/capsules.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { SharedModule } from '@app/shared';
 import { CapsulesRoutingModule } from './capsules-routing.module';
@@ -7,6 +8,6 @@ import { CapsulesPageComponent } from './capsules-page.component';
 
 @NgModule({
   declarations: [CapsulesPageComponent],
-  imports: [CommonModule, SharedModule, CapsulesRoutingModule],
+  imports: [CommonModule, SharedModule, CapsulesRoutingModule, MatTabsModule],
 })
 export class CapsulesModule {}
