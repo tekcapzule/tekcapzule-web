545a64fbe319fc1e9ae70a8307deb50097d02c4e
Prabhu <adithya.prabhu@sap.com>
2021-08-07 14:31:40 +0530

    twitter widget now loading after navigation back to explore page

--- src/app/topics/components/explore-topics/explore-topics.component.ts
+++ src/app/topics/components/explore-topics/explore-topics.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare var twttr: any;
 @Component({
   selector: 'app-explore-topics',
   templateUrl: './explore-topics.component.html',
   styleUrls: ['./explore-topics.component.scss']
 })
-export class ExploreTopicsComponent implements OnInit {
+export class ExploreTopicsComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    twttr.widgets.load();
+}
 }
