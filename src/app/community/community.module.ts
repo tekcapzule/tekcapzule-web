d7bf1f6c1b639ccb9815529f8ea471d7c9a3cb48
Prabhu <adithya.prabhu@sap.com>
2021-08-10 14:53:04 +0530

    community page - minor refactoring

--- src/app/community/community.module.ts
+++ src/app/community/community.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@app/shared';
 import { CommunityRoutingModule } from './community-routing.module';
 import { CommunityPageComponent } from './community-page.component';
+import { HeroCarouselComponent } from './components/hero-carousel/hero-carousel.component';
 
 @NgModule({
-  declarations: [CommunityPageComponent],
-  imports: [CommonModule, SharedModule, CommunityRoutingModule],
+  declarations: [
+    CommunityPageComponent,
+    HeroCarouselComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    CommunityRoutingModule
+  ],
 })
-export class CommunityModule {}
+export class CommunityModule { }
