67bb9b9418c33522c20e31301b37886205e95755
Akhil PB <akhil.pb001@gmail.com>
2021-11-04 23:38:47 +0530

    Added api gateway for services and fixed topic and user api path formation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
   apiStageEnv: 'prod',
+  capsuleApiGateway: 'p94esvj767',
+  eventApiGateway: 'hhp1r4vval',
+  feedbackApiGateway: 'ldy6ilti35',
+  subscriptionApiGateway: 'ppxetjwise',
   topicApiGateway: 'bu3dkcc0t5',
+  userApiGateway: '1p5xohsuk9',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   apiStageEnv: 'dev',
+  capsuleApiGateway: 'xgxtvag6og',
+  eventApiGateway: '3xeph8nun8',
+  feedbackApiGateway: 'hgd9ehej67',
+  subscriptionApiGateway: '0x28a8vs38',
   topicApiGateway: 'odl3njp8ya',
+  userApiGateway: 'rki9lzz4gl',
 };
 
 /*
