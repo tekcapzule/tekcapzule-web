5434464f61d67e4b53a80d13d0f415480b3d8735
Akhil PB <akhil.pb001@gmail.com>
2021-11-05 12:21:07 +0530

    Fixed topic card spacing issue in topics page

--- src/app/shared/components/topic-card/topic-card.component.ts
+++ src/app/shared/components/topic-card/topic-card.component.ts
@@ -3,14 +3,12 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-topic-card',
   templateUrl: './topic-card.component.html',
-  styleUrls: ['./topic-card.component.scss']
+  styleUrls: ['./topic-card.component.scss'],
 })
 export class TopicCardComponent implements OnInit {
   @Input() topic!: any;
-  constructor() { }
 
-  ngOnInit(): void {
-    
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
