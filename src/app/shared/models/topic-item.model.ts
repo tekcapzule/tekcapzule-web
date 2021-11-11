06e3b1ba943cc6893e77ed39ef96bf80c7d74df0
Akhil PB <akhil.pb001@gmail.com>
2021-11-11 13:43:48 +0530

    Api integrated for browse by topics in capsule page

---
+++ src/app/shared/models/topic-item.model.ts
@@ -0,0 +1,11 @@
+export interface TopicItem {
+  code: string;
+  name: string;
+  category: string;
+  description: string;
+  imageUrl: string;
+  aliases: string[];
+  keyHighlights: string[];
+  capsules: string[];
+  status: string;
+}
