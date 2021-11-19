2f6afef3d7d832e4ed53d6a0d60cbec0282b1471
Akhil PB <akhil.pb001@gmail.com>
2021-11-19 14:46:49 +0530

    Code formatting

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -16,20 +17,19 @@ import { AdminTopicsComponent } from './components/admin-topics/admin-topics.com
 import { AdminCreateCapsuleComponent } from './components/admin-create-capsule/admin-create-capsule.component';
 import { AdminCreateTopicComponent } from './components/admin-create-topic/admin-create-topic.component';
 import { AdminCreateQuestionsComponent } from './components/admin-create-questions/admin-create-questions.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AdminPageComponent,
     AdminCapsulesComponent,
     AdminTopicsComponent,
-    // AdminFeedbackComponent,
     AdminCreateCapsuleComponent,
     AdminCreateTopicComponent,
     AdminCreateQuestionsComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
@@ -38,7 +38,6 @@ import { FormsModule } from '@angular/forms';
     MatTabsModule,
     SharedModule,
     AdminRoutingModule,
-    FormsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
