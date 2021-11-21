3226f6e19ce8dda20c945d8123925620186199c5
Akhil PB <akhil.pb001@gmail.com>
2021-11-21 20:38:40 +0530

    Fixed follow and unfollow of topic in topics explore and details page

--- src/app/topics/components/topic-details/topic-details.component.ts
+++ src/app/topics/components/topic-details/topic-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
 
-import { CapsuleItem, TopicItem } from '@app/shared/models';
-import { CapsuleApiService } from '@app/core';
+import { CapsuleApiService, UserApiService } from '@app/core';
+import { CapsuleItem, TopicItem, UserInfo } from '@app/shared/models';
+import { TopicService } from '@app/topics/services/topic.service';
 
 @Component({
   selector: 'app-topic-details',
@@ -12,11 +13,17 @@ import { CapsuleApiService } from '@app/core';
 export class TopicDetailsComponent implements OnInit {
   topic: TopicItem;
   firstThreeCapsules: CapsuleItem[] = [];
+  userInfo: UserInfo = null;
 
-  constructor(private capsuleApi: CapsuleApiService) {}
+  constructor(
+    private capsuleApi: CapsuleApiService,
+    private userApi: UserApiService,
+    private topicService: TopicService
+  ) {}
 
   ngOnInit(): void {
     this.topic = history.state.topic;
+    this.userInfo = this.userApi.getUserCache();
 
     if (this.topic && this.topic.capsules.length > 0) {
       const threeCapsuleIds = this.topic.capsules.slice(0, 3);
@@ -30,5 +37,17 @@ export class TopicDetailsComponent implements OnInit {
         this.firstThreeCapsules = data;
       });
     }
+  }
+
+  isFollowingTopic(): boolean {
+    return this.topicService.isFollowingTopic(this.topic.code);
+  }
+
+  followTopic(): void {
+    this.topicService.followTopic(this.topic.code);
+  }
+
+  unfollowTopic(): void {
+    this.topicService.unfollowTopic(this.topic.code);
   }
 }
