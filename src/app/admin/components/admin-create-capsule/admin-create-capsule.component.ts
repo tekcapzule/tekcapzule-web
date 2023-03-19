f467fab8dea2b598f40481ca05decb32853df260
sathish257216 <sathish.257216@gmail.com>
2023-03-18 20:05:33 -0400

    create capsule changes and footer hidden for Admin pages

--- src/app/admin/components/admin-create-capsule/admin-create-capsule.component.ts
+++ src/app/admin/components/admin-create-capsule/admin-create-capsule.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { CapsuleApiService, ChannelEvent, EventChannelService, AppSpinnerService } from '@app/core';
 import { CreateCapsuleForm } from '@app/admin/models';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-admin-create-capsule',
@@ -14,9 +14,7 @@ export class AdminCreateCapsuleComponent implements OnInit, AfterViewInit {
   capsuleForm: FormGroup;
   createCapsuleForm= new CreateCapsuleForm();
   isCreateCapsuleSubmitted = false;
-  pipe = new DatePipe('en-US');
-  now = Date.now();
-  responseBodySample = {
+  /*responseBodySample = {
     topicCode: 'Cloud Computing',
     publishedDate: '10/10/2022',
     title: 'Block chain',
@@ -34,7 +32,7 @@ export class AdminCreateCapsuleComponent implements OnInit, AfterViewInit {
     expiryDate: '10/10/2022',
     editorsPick: 1,
     tags: ['cld', 'cloud', 'compute', 'storage'],
-  };
+  };*/
 
   constructor(
     private eventChannel: EventChannelService,
@@ -51,19 +49,20 @@ export class AdminCreateCapsuleComponent implements OnInit, AfterViewInit {
     this.capsuleForm = this.fb.group({
       topicCode: ['Cloud Computing', Validators.required],
       category: [''],
-      publishedDate: [this.pipe.transform(this.now, 'dd/MM/yyyy'), Validators.required],
+      publishedDate: [moment().format('DD/MM/YYYY'), Validators.required],
       title:  ['Block chain', Validators.required],
-      imageUrl:  ['http://tekcapsule-web-dev.s3-website.us-east-2.amazonaws.com/assets/images/card-3.png', Validators.required],
+      imageUrl:  ['https://tekcapsule-web-dev.s3-website.us-east-2.amazonaws.com/assets/images/card-3.png', Validators.required],
       duration:  ['120', Validators.required],
       author:  ['Linjith Kunnon', Validators.required],
       description:  ['Sed ut perspiciatis unde omnis iste natus error sit qsiuss voluptatem accusantium doloremque laudantium, sdda sdftotam rem aperiam, eaque ipsa quae ab illo quae ainventore veritatis et quasi architecto beatae vitae quia dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia uuntur magni dolores eos qui ratione voluptatem sequi. sed quia uuntur magni dolores eos qui ratione voluptatem sequi.', Validators.required],
       publisher:  ['Medium', Validators.required],
-      resourceURL:  ['url', Validators.required],
+      resourceURL:  ['https://tekcapsule-web-dev.s3-website.us-east-2.amazonaws.com/assets/images/card-3.png', Validators.required],
       type:  ['ARTICLE', Validators.required],
       audience:  ['ALL'],
       level:  ['ADVANCED', Validators.required],
-      expiryDate:  ['10/10/2022', Validators.required],
-      editorsPick:  ['1'],
+      expiryDate:  [moment().format('DD/MM/YYYY'), Validators.required],
+      expiryDateDisp:  [7, Validators.required],
+      editorsPick:  [true],
       tags:  [ ['cld', 'cloud', 'compute', 'storage']]
     })
   }
@@ -75,17 +74,18 @@ export class AdminCreateCapsuleComponent implements OnInit, AfterViewInit {
   }
 
   onCreateCapsule(): void {
-    console.log(' --->>>> ', this.capsuleForm.valid);
-    console.log(' --->>>> ', this.capsuleForm.value);
-    Object.keys(this.capsuleForm.controls).forEach(key => {
-      console.log(key, this.capsuleForm.get(key).valid, this.capsuleForm.get(key).value);
-    });
+    // Testing Purpose
+    /*Object.keys(this.capsuleForm.controls).forEach(key => {
+      if(!this.capsuleForm.get(key).valid) {
+        console.log(key, this.capsuleForm.get(key).valid, this.capsuleForm.get(key).value);
+      }
+    });*/
     if (this.capsuleForm.valid) {
       let requestBody = this.capsuleForm.value;
-      requestBody.expiryDate = this.pipe.transform(this.now, 'dd/MM/yyyy');
+      requestBody.expiryDate = moment().add(requestBody.expiryDateDisp, 'days').format("DD/MM/YYYY");
+      requestBody.editorsPick = requestBody.editorsPick ? 1 : 0;
       this.isCreateCapsuleSubmitted = false;
       this.appSpinnerService.show();
-      requestBody.editorsPick = this.createCapsuleForm.editorsPick ? 1 : 0;
       console.log('request  ', requestBody);
       this.capsuleApi.createCapsule(requestBody).subscribe(data => {
         this.capsuleForm.reset();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChannelEvent, EventChannelService } from './core';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  destroy$ = new Subject<boolean>();
+  footerHidden: boolean;
+
+  constructor(private eventChannel: EventChannelService) {}
+
+  ngOnInit(): void {
+    this.eventChannel.getChannel().pipe(
+    filter(out => out.event === ChannelEvent.HideAdminNavTabs), takeUntil(this.destroy$))
+    .subscribe(() => {
+      this.footerHidden = true;
+    });
+  }
+}
