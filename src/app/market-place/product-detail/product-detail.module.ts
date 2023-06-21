3494ae38b287cb3ef76cae4df630ea89a509d77d
sathish257216 <sathish.257216@gmail.com>
2023-06-21 16:57:12 -0400

    product detail

---
+++ src/app/market-place/product-detail/product-detail.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { SharedModule } from '@app/shared';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailRoutingModule } from './product-detail-routing.module';
+
+@NgModule({
+  declarations: [ProductDetailComponent],
+  imports: [CommonModule, SharedModule, MatTabsModule, ProductDetailRoutingModule, CheckboxModule],
+})
+export class ProductDetailModule {}
