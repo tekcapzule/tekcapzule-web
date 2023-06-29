0bd65b952428c43268cde3e1ee84287dd109f89c
Sasikumar P <sasi.kumar@mantle3d.com>
2023-06-29 18:07:49 +0530

    Courses HTML

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,9 +5,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { SharedModule } from '@app/shared';
 import { CoursesComponent } from './courses.component';
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, SharedModule, MatTabsModule, CoursesRoutingModule],
+  imports: [CommonModule, SharedModule, MatTabsModule, CoursesRoutingModule, CheckboxModule],
 })
 export class CoursesModule {}
