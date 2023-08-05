1d08bd0a2419cbb1c1241b80d8e3428208fed55f
Akhil PB <apb@cloudera.com>
2023-08-05 16:45:33 +0530

    Added skeleton for SkillStudio Courses page

---
+++ src/app/courses/course-skeleton/course-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-skeleton',
+  templateUrl: './course-skeleton.component.html',
+  styleUrls: ['./course-skeleton.component.scss']
+})
+export class CourseSkeletonComponent {
+
+}
