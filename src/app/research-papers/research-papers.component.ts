91a96d84b177be0de54698603165fb3783501db3
Akhil PB <akhil.pb001@gmail.com>
2023-08-08 23:04:00 +0530

    Skeleton loader for research paper page (#243)
    * Skeleton loader for research paper page
    
    * Resolved merge conflicts

--- src/app/research-papers/research-papers.component.ts
+++ src/app/research-papers/research-papers.component.ts
@@ -9,7 +9,6 @@ import { IResearchPaperDetail } from '@app/shared/models/research-item.model';
 import * as moment from 'moment';
 import { MessageService } from 'primeng/api';
 
-
 @Component({
   selector: 'app-research-papers',
   templateUrl: './research-papers.component.html',
@@ -22,12 +21,13 @@ export class ResearchPapersComponent implements OnInit {
   topics: TopicItem[] = [];
   selectedTopics: string[] = [];
 
-  
-  constructor(private spinner: AppSpinnerService,
+  constructor(
+    public spinner: AppSpinnerService,
     private researchApi: ResearchApiService,
     private helperService: HelperService,
     private router: Router,
-    private messageService: MessageService) {}
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.spinner.show();
@@ -36,7 +36,7 @@ export class ResearchPapersComponent implements OnInit {
       this.spinner.hide();
       this.researchList = data;
       this.researchList.forEach(rl => {
-        rl.publishedOn = moment(rl.publishedOn, 'DD/MM/YYYY').fromNow()
+        rl.publishedOn = moment(rl.publishedOn, 'DD/MM/YYYY').fromNow();
       });
       this.filteredResearchList = data;
     });
@@ -48,7 +48,9 @@ export class ResearchPapersComponent implements OnInit {
       sessionStorage.setItem('com.tekcapsule.pageURL', this.router.url);
       sessionStorage.setItem('com.tekcapsule.resourceURL', research.resourceUrl);
       sessionStorage.setItem('com.tekcapsule.title', research.title);
-      this.router.navigateByUrl('/ai-hub/' + research.researchPaperId +'/detail?pageId=Research_Papers');
+      this.router.navigateByUrl(
+        '/ai-hub/' + research.researchPaperId + '/detail?pageId=Research_Papers'
+      );
     } else {
       window.open(research.resourceUrl, '_blank');
     }
@@ -68,15 +70,18 @@ export class ResearchPapersComponent implements OnInit {
 
   onSearch() {
     let tempList = [...this.researchList];
-    if(this.selectedTopics.length > 0) {
+    if (this.selectedTopics.length > 0) {
       tempList = tempList.filter(research => this.selectedTopics.includes(research.topicCode));
     }
-    if(this.searchText && this.searchText.trim().length > 0) {
-      this.filteredResearchList = tempList.filter(research => this.helperService.getIncludesStr(research.title, this.searchText) 
-      || this.helperService.getIncludesStr(research.topicCode, this.searchText)
-      || this.helperService.getIncludesStr(research.summary, this.searchText)
-      || this.helperService.getIncludesStr(research.description, this.searchText)
-      || this.helperService.getIncludesStr(research.tags.toString(), this.searchText));
+    if (this.searchText && this.searchText.trim().length > 0) {
+      this.filteredResearchList = tempList.filter(
+        research =>
+          this.helperService.getIncludesStr(research.title, this.searchText) ||
+          this.helperService.getIncludesStr(research.topicCode, this.searchText) ||
+          this.helperService.getIncludesStr(research.summary, this.searchText) ||
+          this.helperService.getIncludesStr(research.description, this.searchText) ||
+          this.helperService.getIncludesStr(research.tags.toString(), this.searchText)
+      );
     } else {
       this.filteredResearchList = tempList;
     }
@@ -84,7 +89,7 @@ export class ResearchPapersComponent implements OnInit {
 
   onChange(eve) {
     this.selectedTopics = [];
-    if(eve.value.length > 0) {
+    if (eve.value.length > 0) {
       eve.value.forEach((topic: TopicItem) => this.selectedTopics.push(topic.code));
     }
     this.onSearch();
