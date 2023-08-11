7f22fcaf9a0e73b71d0b6050147d1b92f9fb0298
Akhil PB <apb@cloudera.com>
2023-08-11 20:53:33 +0530

    Added skeleton loader for marketplace page

--- src/app/market-place/market-place.module.ts
+++ src/app/market-place/market-place.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '@app/shared';
 import { MarketPlaceComponent } from './market-place.component';
 import { MarketPlaceRoutingModule } from './market-place-routing.module';
 import { CheckboxModule } from 'primeng/checkbox';
+import { ProductSkeletonComponent } from './product-skeleton/product-skeleton.component';
 
 @NgModule({
-  declarations: [MarketPlaceComponent],
+  declarations: [MarketPlaceComponent, ProductSkeletonComponent],
   imports: [CommonModule, SharedModule, MatTabsModule, MarketPlaceRoutingModule, CheckboxModule],
 })
 export class MarketPlaceModule {}
