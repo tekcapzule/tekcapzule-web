2bd255b60023ab97c75bf13daf7d5c46cf6fd4c7
sathish257216 <sathish.257216@gmail.com>
2023-08-13 11:38:14 +0530

    bread crumb navigation

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -93,7 +93,8 @@ export class HeaderComponent implements OnInit {
     this.router.events.subscribe(ev => {
       if (ev instanceof NavigationStart) {
         window.scrollTo(0, 0);
-        if (!this.selectedMenuItem || !this.helperService.getSelectedMenu()) {
+        //console.log(' -------->> ', ev, ev.url);
+        if (!this.selectedMenuItem || !this.helperService.getSelectedMenu() || !this.selectedMenuItem.navUrl.includes(ev.url)) {
           const selectedMenu = this.helperService.findSelectedMenu(ev.url);
           this.selectedMenuItem = selectedMenu.selectedMenuItem;
           this.selectedChildMenuItem = selectedMenu.selectedChildMenuItem;
