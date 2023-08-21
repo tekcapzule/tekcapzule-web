3d6bb04edfe0d3c211a2e13a6bc9e55e0285990e
sathish257216 <sathish.257216@gmail.com>
2023-08-21 22:44:13 +0530

    merge main

--- src/app/subscription/contact-sales/contact-sales.component.ts
+++ src/app/subscription/contact-sales/contact-sales.component.ts
@@ -3,12 +3,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-contact-sales',
   templateUrl: './contact-sales.component.html',
-  styleUrls: ['./contact-sales.component.scss'],
+  styleUrls: ['./contact-sales.component.scss']
 })
 export class ContactSalesComponent {
   @Input() isDialogVisible = false;
   @Output() dialogClosed = new EventEmitter();
 
+  
   onDialogClosed() {
     this.dialogClosed.emit();
   }
