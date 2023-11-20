73ea9bf7e39d9fe8629d6aae12fbf3af632b2712
sathish257216 <sathish.257216@gmail.com>
2023-11-20 15:50:51 -0500

    Events link

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventChannelService } from '@app/core';
+import { HelperService } from '@app/core/services/common/helper.service';
 import { NavTab } from '@app/shared/models';
 import { ChannelEvent } from '@app/shared/models/channel-item.model';
 import { Constants } from '@app/shared/utils';
@@ -13,9 +14,11 @@ export class FooterComponent implements OnInit {
   menuItems: NavTab[] = [];
   selectedMenuItem: NavTab;
   selectedChildMenuItem: NavTab;
+  eventsPageLink: NavTab;
   
   constructor(
-    private eventChannel: EventChannelService
+    private eventChannel: EventChannelService,
+    private helperService: HelperService
     ) {}
 
   ngOnInit(): void {
@@ -23,6 +26,7 @@ export class FooterComponent implements OnInit {
       const menuitem = Constants.HeaderMenu.find(hm => hm.uniqueId === item);
       this.menuItems.push(menuitem);
     });
+    this.eventsPageLink = this.helperService.findPage('events');
   }
 
   onMenuClick(navTab: NavTab): void {
