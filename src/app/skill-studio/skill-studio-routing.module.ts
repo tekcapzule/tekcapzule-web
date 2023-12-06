20aac56ed089b3808d8c48d87587ea958493b96a
sathish257216 <sathish.257216@gmail.com>
2023-12-06 00:22:29 -0500

    removed dashboard

--- src/app/skill-studio/skill-studio-routing.module.ts
+++ src/app/skill-studio/skill-studio-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SkillStudioComponent } from './skill-studio.component';
-import { AIDashboardComponent } from './ai-dashboard/ai-dashboard.component';
 import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
@@ -9,10 +8,6 @@ const routes: Routes = [
     path: '',
     component: SkillStudioComponent,
     children: [
-      {
-        path: 'dashboard',
-        component: AIDashboardComponent
-      },
       {
         path: ':id/detail',
         component: DetailComponent
